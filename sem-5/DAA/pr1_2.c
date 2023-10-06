d4fa7583e0f7116c66176758ac6d31688e331213
yash <yashsuthar0309@gmail.com>
2023-10-06 21:14:49 +0530

    pr1.2 added

---
+++ sem-5/DAA/pr1_2.c
@@ -0,0 +1,43 @@
+ /*Implementation and Time analysis of linear and binary search algorithms */
+
+// Solution (Binary Search):
+
+#include <stdio.h>
+#include <conio.h>
+int binary_search(int arr[], int num, int l, int h)
+{
+int mid=(l+h)/2;
+if(arr[mid]==num)
+{
+return mid;
+}
+else if(num<arr[mid])
+{
+return binary_search(arr,num,l,mid-1);
+}
+else
+{
+return binary_search(arr,num,mid+1,h);
+}
+}
+void main()
+{
+int arr[100],i,j,n,num,search;
+printf("Enter the size of array : ");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("Enter a value : ");
+scanf("%d",&arr[i]);
+}
+printf("Enter the number to be searched : ");
+scanf("%d",&num);
+
+3
+
+IU2141230297 DAA(CE0516) CSE5-E1
+int l=0;
+search=binary_search(arr,num,l,n);
+printf("Element %d found at %d position",arr[search],search+1);
+getch();
+}
